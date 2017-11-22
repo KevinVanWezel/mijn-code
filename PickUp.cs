8ca413bbb702869c2e0f1c6f9d3aeccf5d472d2f
KevinVanWezel <21973@ma-web.nl>
2017-11-22 15:48:41 +0100

    s

---
+++ PickUp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "pickup")
+        {
+           
+            Destroy(other.gameObject);
+
+           
+        }
+    }
+}
---
+++ PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    private PlayerInput _input;
+    private Rigidbody _rigidbody;
+    private Vector3 _inputDirection;
+
+    void Start()
+    {
+        _input = GetComponent<PlayerInput>();
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        float x = _input.getXspeed();
+        float z = _input.getZspeed();
+        _inputDirection = new Vector3(x, 0f, z).normalized;
+    }
+    void FixedUpdate()
+    {
+        _rigidbody.AddForce(_inputDirection * speed);
+    }
+   
+    
+   
+}
